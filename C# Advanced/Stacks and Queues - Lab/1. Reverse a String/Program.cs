cf32fce2985da2d068ce3647dccaeb43df222c71
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-04-06 22:46:47 +0300

    Added 01, 02 exercise

--- C# Advanced/Stacks and Queues - Lab/1. Reverse a String/Program.cs
+++ C# Advanced/Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _01_Reverse_a_String
+
+namespace _01_Reverse_A_String
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
 
+            Stack<char> stack = new Stack<char>();
 
+            foreach (char character in input)
+            {
+                stack.Push(character);
+            }
 
+            //Console.WriteLine(string.Join("", stack)); <--- Without While
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
         }
     }
 }
--- C# Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
+++ C# Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02_Stack_Sum
 {
@@ -6,10 +7,41 @@ namespace _02_Stack_Sum
     {
         static void Main(string[] args)
         {
+            List<int> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
 
+            Stack<int> stack = new Stack<int>(list);
 
+            string input = string.Empty;
 
+            while (!(input = Console.ReadLine()).Contains("end", StringComparison.OrdinalIgnoreCase))
+            {
+                
+                string[] operation = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
+                string command = operation[0];
+
+                if (command.Contains("add", StringComparison.OrdinalIgnoreCase))
+                {
+                    int firstNumber = int.Parse(operation[1]);
+                    int secondNumber = int.Parse(operation[2]);
+
+                    stack.Push(firstNumber);
+                    stack.Push(secondNumber);
+                }
+                if (command.Contains("remove", StringComparison.OrdinalIgnoreCase))
+                {
+                    int count = int.Parse(operation[1]);
+
+                    if (count <= stack.Count)
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                }
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
         }
     }
 }
