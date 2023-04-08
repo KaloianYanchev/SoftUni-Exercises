da9dbb48f621c5dbb13781c01ec1d7b0910daa89
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-04-08 17:06:36 +0300

    Added Matching Brackets

--- C# Advanced/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
+++ C# Advanced/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _03_Matching_Brackets
+namespace _04_Matching_Brackets
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            Stack<int> stack = new Stack<int>();
 
+            for (int i = 0; i < input.Length; i++) 
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    string substring = string.Empty;
 
+                    substring = input[stack.Pop()..(i+1)]; //<-- Sugar syntax for one below
 
+                    //int length = i - stack.Peek() + 1;
+                    //substring = input.Substring(stack.Pop(), length);
+
+                    Console.WriteLine(substring);
+                }
+            }
         }
     }
 }
