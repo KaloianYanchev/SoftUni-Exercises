06a26e1b72cb5ae69461376b34df7e7c8dbc7c6d
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-04-13 18:09:28 +0300

    Added ex 5

---
+++ C# Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] boxOfClothes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int initialRackCapacity = int.Parse(Console.ReadLine());
+            int currentRackCapacity = 0;
+
+            Stack<int> stack = new Stack<int>(boxOfClothes);
+            int numberOfRacksNeeded = 0;
+
+            while (stack.Count > 0)
+            {
+                int currentClothValue = stack.Pop();
+                if (currentClothValue + currentRackCapacity > initialRackCapacity)
+                {
+                    numberOfRacksNeeded++;
+                    stack.Push(currentClothValue);
+                    currentRackCapacity = 0;
+                }
+                else if (currentClothValue <= initialRackCapacity)
+                {
+                    currentRackCapacity += currentClothValue;
+                }
+
+            }
+
+            if (currentRackCapacity > 0)
+            {
+                numberOfRacksNeeded++;
+            }
+
+            Console.WriteLine(numberOfRacksNeeded);
+        }
+    }
+}
