089ce05819d5aa405253c1a1a13852a31503650c
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-05-20 14:49:17 +0300

    Added ex. 7

---
+++ C# Advanced/Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _07_Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long rows = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[rows][];
+
+            pascalTriangle[0] = new long[1] {1};
+
+            for (int row = 1; row <= rows -1; row++)
+            {
+                pascalTriangle[row] = new long[row+1];
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    long sum = 0;
+
+                    if (col == 0)
+                    {
+                        sum = pascalTriangle[row-1][col];
+                    }
+                    else if (col == pascalTriangle[row -1].Length)
+                    {
+                        sum = pascalTriangle[row-1][col-1];
+                    }
+                    else
+                    {
+                        sum = pascalTriangle[row - 1][col] + pascalTriangle[row - 1][col - 1];
+                    }
+
+                    pascalTriangle[row][col] = sum;
+                }
+            }
+
+            for (int row = 0; row < pascalTriangle.Length; row++)
+            {
+                for(int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    Console.Write($"{pascalTriangle[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
