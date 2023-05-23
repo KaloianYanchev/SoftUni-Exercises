6f123c11028b15ca43c6ad2fabf749e5fdbbd46e
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-05-23 19:41:34 +0300

    Added Ex. 6

---
+++ C# Advanced/Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06_Jagged_Array_Manipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+
+            string input = string.Empty;
+
+            while ("End" != (input = Console.ReadLine()))
+            {
+            
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write($"{matrix[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
