d35e4924a5a7076fa524510b3ce1c71c43e989cd
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-05-30 23:33:51 +0300

    Added Rest of Functional Programming Exercises

---
+++ C# Advanced/Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> checkLargerOrEqualSumOfChars = (name, requirement) =>
+            {
+                int charSum = name.Sum(ch => ch);
+
+                return charSum >= requirement;
+            };
+
+            Func<string[], int, Func<string, int, bool>, string> getName = (names, sum, match) =>
+            {
+                foreach (string name in names)
+                {
+                    if (match(name, sum))
+                    {
+                        return name;
+                    }
+                }
+                return default;
+            };
+
+            int n = int.Parse(Console.ReadLine());
+
+            string[] listOfNames = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(getName(listOfNames, n, checkLargerOrEqualSumOfChars));
+        }
+    }
+}
