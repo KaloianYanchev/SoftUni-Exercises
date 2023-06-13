b93bb9a3ee08125592b3b1366174a97dff67bf99
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-06-13 11:46:22 +0300

    Added Truck Tour

--- C# Advanced/Stacks and Queues - Exercise/07. Truck Tour/Program.cs
+++ C# Advanced/Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -7,9 +7,57 @@ namespace _07_Truck_Tour
     {
         static void Main(string[] args)
         {
+            const int litersPerKilometer = 1;
 
+            int count = int.Parse(Console.ReadLine());
 
+            Queue<int[]> pumps = new();
 
+            for (int i = 0; i < count; i++)
+            {
+                int[] pumpInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                pumps.Enqueue(pumpInfo);
+            }
+
+            int startIndex = 0;
+
+            while (true)
+            {
+                bool isComplete = true;
+                int totalLiters = 0;
+
+                foreach (var pump in pumps)
+                {
+                    int liters = pump[0];
+                    int distance = pump[1];
+
+                    totalLiters += liters;
+
+                    if (totalLiters - distance * litersPerKilometer < 0)
+                    {
+                        startIndex++;
+
+                        int[] currentPump = pumps.Dequeue();
+                        pumps.Enqueue(currentPump);
+
+                        isComplete = false;
+
+                        break;
+                    }
+
+                    totalLiters -= distance * litersPerKilometer;
+                }
+
+                if (isComplete)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
         }
     }
 }
