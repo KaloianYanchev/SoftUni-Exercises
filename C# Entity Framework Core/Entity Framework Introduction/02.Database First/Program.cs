8700282f024a5f4a33cc01f5d4eaf0cf37280922
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-11-05 19:36:00 +0200

    Added Entity Framework Introduction Exercises

---
+++ C# Entity Framework Core/Entity Framework Introduction/02.Database First/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            //var employee = context.Employees.Find(2);
+
+            //Console.WriteLine(employee.FirstName);
+
+            //string name = GetEmployeesFullInformation(context);
+            GetEmployeesFullInformation(context);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            DbSet<Employee> employees = context.Employees;
+            Console.WriteLine(employees.ToQueryString());
+
+
+            return "";
+        }
+    }
+}
