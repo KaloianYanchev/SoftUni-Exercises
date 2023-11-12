2c6958cd38e0d81ca6c9b3258b0b9227cada9d47
KaloianYanchev <122824992+KaloianYanchev@users.noreply.github.com>
2023-11-12 19:02:51 +0200

    Added Few ex

--- C# Entity Framework Core/Entity Framework Introduction/02.Database First/Program.cs
+++ C# Entity Framework Core/Entity Framework Introduction/02.Database First/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoftUni.Data;
 using SoftUni.Models;
 
@@ -10,21 +11,83 @@ namespace SoftUni
         {
             SoftUniContext context = new SoftUniContext();
 
-            //var employee = context.Employees.Find(2);
-
-            //Console.WriteLine(employee.FirstName);
-
-            //string name = GetEmployeesFullInformation(context);
-            GetEmployeesFullInformation(context);
+            //Console.WriteLine(GetEmployeesFullInformation(context));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            //Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+            Console.WriteLine(AddNewAddressToEmployee(context));
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
-            DbSet<Employee> employees = context.Employees;
-            Console.WriteLine(employees.ToQueryString());
+            //DbSet<Employee> employees = context.Employees;
+            
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                }).ToList();
+
+            string result = string.Join(Environment.NewLine, 
+                employees.Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+
+            return result;
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+
+            return result;
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select( e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} {e.LastName} from {e.Name} - ${e.Salary:f2}"));
+
+            return result;
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            string result = "";
+            var adress = new Address
+            {
+                TownId = 4,
+                AddressText = "Vitoshka 15"
+            };
+
+            Console.WriteLine(adress.TownId);
+            Console.WriteLine(adress.AddressText);
 
 
-            return "";
+            return result;
         }
     }
 }
